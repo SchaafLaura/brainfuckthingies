1c094f7438ae43c3c67951176baa6e8030e6326b
SchaafLaura <schaaf.laura92@gmail.com>
2024-05-07 13:59:28 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
-use std::env; 
-fn main(){
+use std::env;
+fn main() {
     let args: Vec<_> = env::args().collect();
     match args.len() {
-        0 | 1 => {eprintln!("no instructions found");},
-        _ => parse(args[1].clone())
+        0 | 1 => {
+            eprintln!("no instructions found");
+        }
+        _ => parse(args[1].clone()),
     }
 }
 
-fn parse(instructions: String){
-    let mut data: Vec<u8> = vec![0; 30_000];
-    let mut data_ptr: i32 = 0;
-    let mut inst_ptr: i32 = 0;
-    while inst_ptr < instructions.len() as i32{
+// brb
 
+/*
+> 	Increment the data pointer by one (to point to the next cell to the right).
+< 	Decrement the data pointer by one (to point to the next cell to the left).
++ 	Increment the byte at the data pointer by one.
+- 	Decrement the byte at the data pointer by one.
+. 	Output the byte at the data pointer.
+, 	Accept one byte of input, storing its value in the byte at the data pointer.
+[ 	If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+] 	If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.[a]
+*/
+
+fn parse(instructions: String) {
+    let mut data: Vec<u8> = vec![0; 30_000];
+    let mut data_ptr: usize = 0;
+    let mut inst_ptr: usize = 0;
+
+    let isnt: Vec<u8> = instructions.chars().map(|x| x as u8).collect();
+
+    while inst_ptr < instructions.len() {
+        match isnt[inst_ptr] as char {
+            '+' => data[data_ptr] += 1,
+            '-' => data[data_ptr] -= 1,
+            '>' => data_ptr += 1,
+            '<' => data_ptr -= 1,
+            '.' => print!("{}", data[data_ptr] as char),
+            ',' => todo!(), // read user input
+            '[' => jump(true),
+            ']' => jump(false),
+            _ => todo!(),
+        }
+        inst_ptr += 1
+    }
+
+    fn jump(dir: bool, ptr: &i32, inst: &Vec<u8>) {
+        let mut d: i32 = 0;
+
+        let other= if dir {']'} else {'['};
+
+        loop {
+            match (inst[ptr] as char, d) {
+                (other, 0) => //,
+
+            }
+        }
     }
 }
