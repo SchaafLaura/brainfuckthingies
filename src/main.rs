a25e286d47b04830cd262ad102c0f561295a1f87
SchaafLaura <schaaf.laura92@gmail.com>
2024-05-07 12:57:24 +0200

    mainrus

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use std::env; 
 fn main(){
     let args: Vec<_> = env::args().collect();
+    match args.len() {
+        0 | 1 => {eprintln!("no instructions found");},
+        _ => parse(args[1].clone())
+    }
+}
+
+fn parse(instructions: String){
     let mut data: Vec<u8> = vec![0; 30_000];
     let mut data_ptr: i32 = 0;
     let mut inst_ptr: i32 = 0;
-
-    let instructions: String = if args.len() > 1 {
-        args[1].clone()
-    } else {
-        panic!("no instructions found");
-    };
-
     while inst_ptr < instructions.len() as i32{
 
     }
-    
 }
